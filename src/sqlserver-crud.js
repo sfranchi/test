6d799ee47e275a30ce65e22e5a673a7f00cf53b9
Sandro Franchi <sandrof@lagash.com>
2018-04-23 15:53:01 -0300

    Handling of private-public methods for this module

--- src/sqlserver-crud.js
+++ src/sqlserver-crud.js
@@ -1,22 +1,27 @@
 'use strict';
 
-var exports = module.exports = {};
 const sqlserver = require('mssql');
 
-exports.create = function(req, res, next) {
+var _create = function(req, res, next) {
 };
 
-exports.retrieve = function(req, res, next) {
+var _retrieve = function(req, res, next) {
     res.setHeader('Content-type', 'application/json');
     res.writeHead(200);
-    res.end(JSON.stringify({driver:'sqlserver', version: req.headers['accept-version']}));
+    res.end(JSON.stringify({driver:'sqlserver', version: req.getVersion()}));
     
     return next();
 };
 
-exports.update = function(req, res, next) {
+var _update = function(req, res, next) {
 };
 
-exports.delete = function(req, res, next) {
+var _delete = function(req, res, next) {
 };
 
+module.exports = {
+    create: _create,
+    retrieve: _retrieve,
+    update: _update,
+    delete: _delete
+}
